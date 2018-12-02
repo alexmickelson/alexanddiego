457505f5554d0bfeeeaf459674912515d59c6ac2
Diego Vanegar Zuniga <diego.vanegaszuniga@ad.snow.edu>
2018-12-01 20:46:01 -0700

    adfg

--- nodeCode/fileupload.js
+++ nodeCode/fileupload.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
+var path = require ("path")
 var bodyParser = require('body-parser');
 var multer = require('multer');
-app.use(express.static(__dirname + '/css'));
-//app.use(express.static('public'));
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 var upload = multer({ dest: '/tmp' })
 
